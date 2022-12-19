2f1f6230b39ae3582af89eaec059a586d2d0ecca
Peter-TMK <tundepeter01@gmail.com>
2022-12-19 15:22:09 +0100

    new: mongoose setup for data persisting

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,31 @@
 // require('dotenv').config()
 const http = require('http')
+const mongoose = require('mongoose');
+
 const app = require('./app')
 
 const { loadPlanetsData } = require('./models/planets.model')
 const PORT = process.env.PORT || 7000;
 
+const MONGO_URL = 'mongodb://localhost:27017/Space_API'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', ()=> {
+    console.log('MongoDb connected successfully!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        // useFindAndModify: false,
+        // useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
     await loadPlanetsData();
 
     server.listen(PORT, ()=>{
